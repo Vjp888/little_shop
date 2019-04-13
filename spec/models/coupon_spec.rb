370183713d00a70ff17ef4d45f60979c94dd9fda
vjp888 <40582072+Vjp888@users.noreply.github.com>
2019-04-13 17:45:48 -0600

    Adds tests for model level integration of coupons into users and orders

---
+++ spec/models/coupon_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Coupon, type: :model do
+  describe 'validations' do
+    it {should validate_uniqueness_of :name}
+    it {should define_enum_for :type}
+    it {should validate_presence_of :amount_off}
+  end
+
+  describe 'relationships' do
+    it {should belong_to :user}
+    it {should have_many :orders}
+  end
+end
